055f9d610baceab796c612910b6480b3143daca8
Jer <jeremy@boldapps.net>
2017-02-15 19:48:44 -0600

    removed object.assign

--- src/index.js
+++ src/index.js
@@ -98,7 +98,11 @@ class ChartComponent extends React.Component {
 
     this.shadowDataProp = {
       ...data,
-      datasets: data.datasets && data.datasets.map(set => Object.assign({}, set))
+      datasets: data.datasets && data.datasets.map(set => {
+        return {
+            ...set
+        }
+      })
     };
   }
 

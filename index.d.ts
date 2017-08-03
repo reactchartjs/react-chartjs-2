ec6cc4f5272394312f647d7d2dde26882fccb668
Nikhil John <nikhil.john.88@gmail.com>
2017-08-03 12:15:20 +0700

    Update typings and package.json types as per latest Typescript specs
    https://www.typescriptlang.org/docs/handbook/declaration-files/publishing.html

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,58 @@
+// Type definitions for react-chartjs-2 2.0
+// Project: https://github.com/gor181/react-chartjs-2
+// Definitions by: Alexandre Paré <https://github.com/apare>
+//                 Fabien Lavocat <https://github.com/FabienLavocat>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
 
-import * as React from 'react';
+import * as React from "react";
+import * as chartjs from "chart.js";
 
-interface ChartComponentProps {
-	data :Object;
-	height? :number;
-	legend? :Object;
-	onElementsClick? :Function;
-	options? :Object;
-	redraw? :boolean;
-	type? :String;
-	width? :number;
+export type ChartDataFunction<T extends chartjs.ChartData> = (element: HTMLElement) => T;
+export type ChartData<T extends chartjs.ChartData> = ChartDataFunction<T> | T;
+
+export interface ChartComponentProps {
+  data: ChartData<chartjs.ChartData>;
+  type?: chartjs.ChartType;
+  getDatasetAtEvent?(e: any): void;
+  getElementAtEvent?(e: any): void;
+  getElementsAtEvent?(e: any): void;
+  height?: number;
+  legend?: chartjs.ChartLegendOptions;
+  onElementsClick?(e: any): void; // alias for getElementsAtEvent (backward compatibility)
+  options?: chartjs.ChartOptions;
+  redraw?: boolean;
+  width?: number;
 }
 
-export class Doughnut extends React.Component<ChartComponentProps, any>{}
-export class Pie extends React.Component<ChartComponentProps, any>{}
-export class Line extends React.Component<ChartComponentProps, any>{}
-export class Bar extends React.Component<ChartComponentProps, any>{}
-export class HorizontalBar extends React.Component<ChartComponentProps, any>{}
-export class Radar extends React.Component<ChartComponentProps, any>{}
-export class Polar extends React.Component<ChartComponentProps, any>{}
-export class Scatter extends React.Component<ChartComponentProps, any>{}
+export interface LinearComponentProps extends ChartComponentProps {
+  data: ChartData<chartjs.ChartData>;
+}
+
+export default class ChartComponent<P extends ChartComponentProps> extends React.Component<P> {
+  chart_instance: chartjs;
+}
+
+export class Doughnut extends ChartComponent<ChartComponentProps> {
+}
+
+export class Pie extends ChartComponent<ChartComponentProps> {
+}
+
+export class Line extends ChartComponent<LinearComponentProps> {
+}
+
+export class Bar extends ChartComponent<LinearComponentProps> {
+}
+
+export class HorizontalBar extends ChartComponent<ChartComponentProps> {
+}
+
+export class Radar extends ChartComponent<ChartComponentProps> {
+}
+
+export class Polar extends ChartComponent<ChartComponentProps> {
+}
+
+export class Bubble extends ChartComponent<ChartComponentProps> {
+}

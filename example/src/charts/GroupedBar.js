e72f026f362654ab67a634afcae0aac57e0f2f2d
Jay Honnold <jayhonnold@gmail.com>
2020-08-01 14:24:09 -0500

    New example

---
+++ example/src/charts/GroupedBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { BarChart } from 'react-chartjs-2'
+
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of Red Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: '# of Blue Votes',
+      data: [2, 3, 20, 5, 1, 4],
+      backgroundColor: 'rgb(54, 162, 235)',
+    },
+    {
+      label: '# of Green Votes',
+      data: [3, 10, 13, 15, 22, 30],
+      backgroundColor: 'rgb(75, 192, 192)',
+    },
+  ],
+}
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+}
+
+const GroupedBar = () => (
+  <>
+    <div className='header'>
+      <h1 className='title'>Grouped Bar Chart</h1>
+      <div className='links'>
+        <a
+          class='btn btn-gh'
+          href='https://github.com/jhonnold/react-chart.js/blob/master/example/src/charts/GroupedBar.js'
+        >
+          Github Source
+        </a>
+      </div>
+    </div>
+    <BarChart data={data} options={options} />
+  </>
+)
+
+export default GroupedBar
---
+++ example/src/charts/Radar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { RadarChart } from 'react-chartjs-2'
+
+const data = {
+  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [2, 9, 3, 5, 2, 3],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+}
+
+const options = {
+  scale: {
+    ticks: { beginAtZero: true },
+  },
+}
+
+const Radar = () => (
+  <>
+    <div className='header'>
+      <h1 className='title'>Radar Chart</h1>
+      <div className='links'>
+        <a
+          class='btn btn-gh'
+          href='https://github.com/jhonnold/react-chart.js/blob/master/example/src/charts/Radar.js'
+        >
+          Github Source
+        </a>
+      </div>
+    </div>
+    <RadarChart data={data} options={options} />
+  </>
+)
+
+export default Radar

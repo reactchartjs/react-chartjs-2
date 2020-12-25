aaeb9bf12a3f9a587933ffbeb5337d482e3ecc37
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2020-12-25 14:01:41 -0800

    update bar.js in function component

--- example/src/components/bar.js
+++ example/src/components/bar.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import {Bar} from 'react-chartjs-2';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 1,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: [95, 59, 80, 81, 56, 55, 80],
+        },
+        {
+            label: "My second dataset",
+            backgroundColor: "rgba(55,109,82,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 1,
+            hoverBackgroundColor: "rgba(55,109,82,0.2)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: [95, 59, 80, 81, 56, 55, 80],
+        },
+    ],
 };
 
-export default React.createClass({
-  displayName: 'BarExample',
-
-  render() {
+function BarChart() {
     return (
-      <div>
-        <h2>Bar Example (custom size)</h2>
-        <Bar
-          data={data}
-          width={100}
-          height={50}
-          options={{
-            maintainAspectRatio: false
-          }}
-        />
-      </div>
+        <div>
+            <h2>Bar Example (custom size)</h2>
+            <Bar
+                data={data}
+                width={100}
+                height={250}
+                options={{
+                    maintainAspectRatio: false,
+                }}
+            />
+        </div>
     );
-  }
-});
+}
+export default BarChart;

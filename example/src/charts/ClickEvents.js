b726a2a9ff24e5ac7271d8fa930dd2ab0fdfaca5
Leonid Yakovlev <leonid@cube.dev>
2021-04-22 12:40:09 +0300

    clear ClickEvents example

--- example/src/charts/ClickEvents.js
+++ example/src/charts/ClickEvents.js
@@ -1,18 +1,7 @@
 import React, { useState } from 'react';
-import { Bar, Chart } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 const rand = () => Math.floor(Math.random() * 255);
-
-const plugins = [
-  {
-    beforeInit: function (chart, args, options) {
-      console.log(chart);
-      console.log(args);
-      console.log(options);
-    },
-  },
-];
-console.log(Chart.register);
 
 const genData = () => ({
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -99,7 +88,6 @@ const ClickEvents = () => {
       <Bar
         data={data}
         options={options}
-        plugins={plugins}
         getDatasetAtEvent={getDatasetAtEvent}
         getElementAtEvent={getElementAtEvent}
         getElementsAtEvent={getElementsAtEvent}

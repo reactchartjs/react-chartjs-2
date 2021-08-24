24afc92e6cd6bb8d19b2cbbb8a7799dfe0acb087
Shimpei Takamatsu <shimpeiws@gmail.com>
2021-08-24 10:47:25 +0900

    Add stack properties

--- example/src/charts/GroupedBar.js
+++ example/src/charts/GroupedBar.js
@@ -8,16 +8,19 @@ const data = {
       label: '# of Red Votes',
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: 'rgb(255, 99, 132)',
+      stack: 'Stack 0',
     },
     {
       label: '# of Blue Votes',
       data: [2, 3, 20, 5, 1, 4],
       backgroundColor: 'rgb(54, 162, 235)',
+      stack: 'Stack 0',
     },
     {
       label: '# of Green Votes',
       data: [3, 10, 13, 15, 22, 30],
       backgroundColor: 'rgb(75, 192, 192)',
+      stack: 'Stack 1',
     },
   ],
 };

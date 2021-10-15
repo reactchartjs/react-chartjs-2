63a86cfbacb1f75a40cc325465d069925b9c48c3
dangreen <danon0404@gmail.com>
2021-10-15 17:03:24 +0700

    refactor: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    '../stories/*.tsx'
+  ],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
+  ],
+}
---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export const theme = create({
+  base: 'light',
+  brandTitle: 'react-chartjs-2',
+  brandUrl: 'https://github.com/reactchartjs/react-chartjs-2',
+});

8027fa8cb0d840f5c5be6093c9261e51f2625331
Dan Onoshko <danon0404@gmail.com>
2021-11-02 15:54:27 +0700

    docs(examples): bubble chart example (#833)

---
+++ sandboxes/bubble/default/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Bubble } from 'react-chartjs-2';
+import faker from 'faker';
+
+export const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+export const data = {
+  datasets: [
+    {
+      label: 'Red dataset',
+      data: Array.from({ length: 50 }, () => ({
+        x: faker.datatype.number({ min: -100, max: 100 }),
+        y: faker.datatype.number({ min: -100, max: 100 }),
+        r: faker.datatype.number({ min: 5, max: 20 }),
+      })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Blue dataset',
+      data: Array.from({ length: 50 }, () => ({
+        x: faker.datatype.number({ min: -100, max: 100 }),
+        y: faker.datatype.number({ min: -100, max: 100 }),
+        r: faker.datatype.number({ min: 5, max: 20 }),
+      })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function App() {
+  return <Bubble options={options} data={data} />;
+}

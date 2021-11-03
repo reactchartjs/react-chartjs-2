abd3a734fd687a0efe4cabd58b049221d2331461
dangreen <danon0404@gmail.com>
2021-11-04 01:43:16 +0700

    refactor: removed chart.js re-exports
    BREAKING CHANGE: Chart.js re-exports are removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-// @todo Remove these exports
-export { default as Chart } from 'chart.js/auto';
-export { defaults } from 'chart.js';
-
 // @todo Make named export instead of default
 export { Chart as default } from './chart';
 export type { ChartProps } from './types';

e0fcdc06de6dd0a8c0fd464c659b8921db3bdabf
dangreen <danon0404@gmail.com>
2022-04-08 03:15:19 +0700

    feat: react 18 support

--- sandboxes/line/default/index.tsx
+++ sandboxes/line/default/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+createRoot(rootElement).render(<App />);

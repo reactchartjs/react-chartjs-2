0f93ce870ba39a7cd7b911fee8f87d7d69f1ef54
Dan Onoshko <danon0404@gmail.com>
2022-11-18 16:31:40 +0400

    docs: move sandboxes to vite (#1111)

--- sandboxes/bar/vertical/index.tsx
+++ sandboxes/bar/vertical/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
+import { App } from './App.js';
 
 const rootElement = document.getElementById('root');
-createRoot(rootElement).render(<App />);
+createRoot(rootElement!).render(<App />);

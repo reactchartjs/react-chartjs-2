07ffdfe680d6a1f3041907d4cc7283c470abac05
Antti Lehto <anajavi@users.noreply.github.com>
2025-01-01 12:52:41 +0200

    fix: test setup typings

--- test/setup.ts
+++ test/setup.ts
@@ -1,19 +1,3 @@
 import '@testing-library/jest-dom/vitest';
 
 import 'vitest-canvas-mock';
-
-class ResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-
-window.ResizeObserver = ResizeObserver;
-
-class MutationObserver {
-  disconnect() {}
-  unobserve() {}
-  observe() {}
-}
-
-window.MutationObserver = MutationObserver;

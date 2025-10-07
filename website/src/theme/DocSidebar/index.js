2ec160ecd733d6142fe8461231126bc291d45d1e
dangreen <danon0404@gmail.com>
2025-10-07 23:40:11 +0400

    docs: banner fix

--- website/src/theme/DocSidebar/index.js
+++ website/src/theme/DocSidebar/index.js
@@ -2,6 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 import OriginalDocSidebar from '@theme-original/DocSidebar';
 
+function reload() {
+  if (typeof ethicalads !== 'undefined') {
+    ethicalads.reload();
+  } else {
+    setTimeout(reload, 300);
+  }
+}
+
 export default function DocSidebar(props) {
   const bannerRef = useRef();
   const { colorMode } = useColorMode();
@@ -16,7 +24,7 @@ export default function DocSidebar(props) {
       banner.setAttribute('data-ea-publisher', 'react-chartjs-2jsorg');
       banner.setAttribute('data-ea-type', 'image');
       document.querySelector('.theme-doc-sidebar-menu')?.parentElement?.appendChild(banner);
-      ethicalads.reload();
+      reload();
     }
   }, []);
 
